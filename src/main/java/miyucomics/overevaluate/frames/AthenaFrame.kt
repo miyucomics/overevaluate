282519f65162cd6b40986bba4154fb059705fa73
miyucomics <nbriannyc@gmail.com>
2025-04-30 21:27:09 -0400

    Fixed athena

--- src/main/java/miyucomics/overevaluate/frames/AthenaFrame.kt
+++ src/main/java/miyucomics/overevaluate/frames/AthenaFrame.kt
@@ -1,42 +1,76 @@
 package miyucomics.overevaluate.frames
 
-import at.petrak.hexcasting.api.casting.eval.vm.CastingVM
-import at.petrak.hexcasting.api.casting.eval.vm.ContinuationFrame
-import at.petrak.hexcasting.api.casting.eval.vm.SpellContinuation
-
 import at.petrak.hexcasting.api.casting.eval.CastResult
 import at.petrak.hexcasting.api.casting.eval.ResolvedPatternType
+import at.petrak.hexcasting.api.casting.eval.vm.CastingVM
+import at.petrak.hexcasting.api.casting.eval.vm.ContinuationFrame
+import at.petrak.hexcasting.api.casting.eval.vm.FrameFinishEval
+import at.petrak.hexcasting.api.casting.eval.vm.SpellContinuation
+import at.petrak.hexcasting.api.casting.eval.vm.SpellContinuation.NotDone
 import at.petrak.hexcasting.api.casting.iota.BooleanIota
 import at.petrak.hexcasting.api.casting.iota.Iota
 import at.petrak.hexcasting.api.casting.iota.NullIota
+import at.petrak.hexcasting.api.casting.iota.PatternIota
 import at.petrak.hexcasting.common.lib.hex.HexEvalSounds
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation
 import net.minecraft.nbt.NbtCompound
 import net.minecraft.server.world.ServerWorld
 
 object AthenaFrame : ContinuationFrame {
-	@JvmField
-	val TYPE: ContinuationFrame.Type<AthenaFrame> = object : ContinuationFrame.Type<AthenaFrame> {
-		override fun deserializeFromNBT(tag: NbtCompound, world: ServerWorld) = AthenaFrame
+	private val TYPE: ContinuationFrame.Type<AthenaFrame> = object : ContinuationFrame.Type<AthenaFrame> {
+		override fun deserializeFromNBT(compound: NbtCompound, world: ServerWorld) = AthenaFrame
 	}
 
 	override val type: ContinuationFrame.Type<*> = TYPE
-	override fun breakDownwards(stack: List<Iota>): Pair<Boolean, List<Iota>> {
-		val newStack = stack.toMutableList()
-		newStack.add(BooleanIota(false))
-		return false to newStack
-	}
 
-	override fun evaluate(continuation: SpellContinuation, world: ServerWorld, harness: CastingVM): CastResult {
+	override fun evaluate(continuation: SpellContinuation, level: ServerWorld, vm: CastingVM): CastResult {
+		val stack = vm.image.stack.toMutableList()
+		stack.add(BooleanIota(false))
 		return CastResult(
 			NullIota(),
 			continuation,
-			null,
+			vm.image.copy(stack = stack),
 			listOf(),
 			ResolvedPatternType.EVALUATED,
 			HexEvalSounds.NOTHING,
 		)
 	}
 
-	override fun size() = 0
+	override fun breakDownwards(stack: List<Iota>): Pair<Boolean, List<Iota>> {
+		val oldStack = stack.toMutableList()
+		oldStack.add(BooleanIota(false))
+		return false to oldStack
+	}
+
 	override fun serializeToNBT() = NbtCompound()
+	override fun size() = 0
+
+	@JvmStatic
+	fun handleAthena(pattern: PatternIota, vm: CastingVM, world: ServerWorld, continuation: SpellContinuation, originalMethod: Operation<CastResult>): CastResult {
+		val original = originalMethod.call(vm, world, continuation)
+		if (original.resolutionType == ResolvedPatternType.EVALUATED)
+			return original
+		val reduced = reduceAthena(continuation) ?: return original
+
+		val stack = vm.image.stack.toMutableList()
+		stack.add(BooleanIota(true))
+		return CastResult(pattern, reduced, vm.image.copy(stack = stack), listOf(), ResolvedPatternType.EVALUATED, HexEvalSounds.NORMAL_EXECUTE)
+	}
+
+	private fun reduceAthena(continuation: SpellContinuation): SpellContinuation? {
+		var cont = continuation
+		while (cont is NotDone) {
+			if (cont.frame is AthenaFrame) {
+				cont = cont.next
+				while (cont is NotDone) {
+					if (cont.frame is FrameFinishEval)
+						return cont.next
+					cont = cont.next
+				}
+			}
+
+			cont = (cont as NotDone).next
+		}
+		return null
+	}
 }
