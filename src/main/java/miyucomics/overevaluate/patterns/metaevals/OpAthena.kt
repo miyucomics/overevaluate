7b8dfff191f93d5225d9780c092143604cabbcb1
miyucomics <nbriannyc@gmail.com>
2025-05-01 21:48:45 -0400

    Neated some code

--- src/main/java/miyucomics/overevaluate/patterns/metaevals/OpAthena.kt
+++ src/main/java/miyucomics/overevaluate/patterns/metaevals/OpAthena.kt
@@ -1,6 +1,5 @@
 package miyucomics.overevaluate.patterns.metaevals
 
-import at.petrak.hexcasting.api.casting.SpellList
 import at.petrak.hexcasting.api.casting.castables.Action
 import at.petrak.hexcasting.api.casting.eval.CastingEnvironment
 import at.petrak.hexcasting.api.casting.eval.OperationResult
@@ -8,29 +7,23 @@ import at.petrak.hexcasting.api.casting.eval.vm.CastingImage
 import at.petrak.hexcasting.api.casting.eval.vm.FrameEvaluate
 import at.petrak.hexcasting.api.casting.eval.vm.FrameFinishEval
 import at.petrak.hexcasting.api.casting.eval.vm.SpellContinuation
-import at.petrak.hexcasting.api.casting.evaluatable
-import at.petrak.hexcasting.api.casting.iota.Iota
-import at.petrak.hexcasting.api.casting.mishaps.MishapNotEnoughArgs
+import at.petrak.hexcasting.api.casting.getList
 import at.petrak.hexcasting.common.lib.hex.HexEvalSounds
 import miyucomics.overevaluate.frames.AthenaFrame
 
 object OpAthena : Action {
 	override fun operate(env: CastingEnvironment, image: CastingImage, continuation: SpellContinuation): OperationResult {
 		val stack = image.stack.toMutableList()
-		val iota = stack.removeLastOrNull() ?: throw MishapNotEnoughArgs(1, 0)
-		return exec(image, continuation, stack, iota)
-	}
+		val instructions = stack.getList(0, 1)
+		stack.removeLastOrNull()
 
-	private fun exec(image: CastingImage, continuation: SpellContinuation, newStack: MutableList<Iota>, iota: Iota): OperationResult {
-		val instrs = evaluatable(iota, 0)
-		val instructionList = instrs.map({ SpellList.LList(0, listOf(it)) }, { it })
 		return OperationResult(
-			image.withUsedOp().copy(stack = newStack),
+			image.withUsedOp().copy(stack = stack),
 			listOf(),
 			continuation
 				.pushFrame(FrameFinishEval)
 				.pushFrame(AthenaFrame)
-				.pushFrame(FrameEvaluate(instructionList, false)),
+				.pushFrame(FrameEvaluate(instructions, false)),
 			HexEvalSounds.HERMES
 		)
 	}
