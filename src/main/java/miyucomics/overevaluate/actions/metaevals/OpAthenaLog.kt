44469b5d347d8ea315814c362ab26e0cf37c45d4
miyucomics <nbriannyc@gmail.com>
2026-01-24 10:28:01 -0500

    Added useful nuance to Athena's Reflection

--- src/main/java/miyucomics/overevaluate/actions/metaevals/OpAthenaLog.kt
+++ src/main/java/miyucomics/overevaluate/actions/metaevals/OpAthenaLog.kt
@@ -6,6 +6,7 @@ import at.petrak.hexcasting.api.casting.eval.OperationResult
 import at.petrak.hexcasting.api.casting.eval.vm.CastingImage
 import at.petrak.hexcasting.api.casting.eval.vm.SpellContinuation
 import at.petrak.hexcasting.api.casting.iota.GarbageIota
+import at.petrak.hexcasting.api.casting.iota.NullIota
 import at.petrak.hexcasting.common.lib.hex.HexEvalSounds
 import miyucomics.hexpose.iotas.TextIota
 import net.fabricmc.loader.api.FabricLoader
@@ -13,17 +14,10 @@ import net.minecraft.text.Text
 
 object OpAthenaLog : Action {
 	override fun operate(env: CastingEnvironment, image: CastingImage, continuation: SpellContinuation): OperationResult {
-		val stack = image.stack.toMutableList()
+		if (!image.userData.contains("last_mishap"))
+			return OperationResult(image.withUsedOp().copy(stack = image.stack.plus(NullIota())), listOf(), continuation, HexEvalSounds.NORMAL_EXECUTE)
 
-		if (FabricLoader.getInstance().isModLoaded("hexpose")) {
-			var output = Text.literal("No mishap.")
-			if (image.userData.contains("last_mishap"))
-				output = Text.Serializer.fromJson(image.userData.getString("last_mishap"))!!
-			stack.add(TextIota(output))
-		} else {
-			stack.add(GarbageIota())
-		}
-
-		return OperationResult(image.withUsedOp().copy(stack = stack), listOf(), continuation, HexEvalSounds.NORMAL_EXECUTE)
+		val newIota = if (FabricLoader.getInstance().isModLoaded("hexpose")) TextIota(Text.Serializer.fromJson(image.userData.getString("last_mishap"))!!) else GarbageIota()
+		return OperationResult(image.withUsedOp().copy(stack = image.stack.plus(newIota)), listOf(), continuation, HexEvalSounds.NORMAL_EXECUTE)
 	}
 }
